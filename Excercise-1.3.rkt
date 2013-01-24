467b82969ec365a5fb7223d5b9f1113dcaee6b8e
Jake Burton <jake5991@live.com>
2013-01-24 22:25:13 +0000

    Ex1.3

---
+++ Excercise-1.3.rkt
@@ -0,0 +1,5 @@
+#lang R5RS
+(define (big-square a b c)
+    (cond ((and (> a c)(> b c)) (+ (* a a)(* b b)))
+          ((and (> a b)(> c b)) (+ (* a a)(* c c)))
+          ((and (> b a)(> c a)) (+ (* b b)(* c c)))))
